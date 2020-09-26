1fdf903f1438006ebcdb121bc0762f556ce0ac0d
DewinP <dewinspena@gmail.com>
2020-09-26 08:21:12 -0400

    late repository

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { __PROD__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { User } from "./entities/user";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
+  entities: [Post, User],
+  dbName: "reddit",
+  type: "postgresql",
+  debug: !__PROD__,
+} as Parameters<typeof MikroORM.init>[0];
